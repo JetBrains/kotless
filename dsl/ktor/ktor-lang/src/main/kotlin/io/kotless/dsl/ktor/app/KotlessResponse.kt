8f5e935fe4ec8c4b2020a9cbf9b3c4faf513c9f0
Vladislav Tankov <vdtankov@gmail.com>
2019-10-24 13:27:38 +0300

    Hot fix for problem with Kotless Response

--- dsl/ktor/ktor-lang/src/main/kotlin/io/kotless/dsl/ktor/app/KotlessResponse.kt
+++ dsl/ktor/ktor-lang/src/main/kotlin/io/kotless/dsl/ktor/app/KotlessResponse.kt
@@ -7,14 +7,27 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.OutgoingContent
 import io.ktor.response.ResponseHeaders
 import io.ktor.server.engine.BaseApplicationResponse
-import io.ktor.util.toByteArray
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.io.*
 import sun.reflect.generics.reflectiveObjects.NotImplementedException
+import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
-class KotlessResponse(call: ApplicationCall) : BaseApplicationResponse(call) {
+class KotlessResponse(call: ApplicationCall) : BaseApplicationResponse(call), CoroutineScope {
+    override val coroutineContext: CoroutineContext = EmptyCoroutineContext
     private val output = ByteChannel(true)
+    private val bytes = ArrayList<Byte>()
+
+    val reader = launch(Dispatchers.Unconfined) {
+        val packet = output.readRemaining()
+        var res = ByteArray(4096)
+        var size = packet.readAvailable(res)
+        while (size != 0) {
+            bytes.addAll(res.toList().take(size))
+            res = ByteArray(4096)
+            size = packet.readAvailable(res)
+        }
+    }
 
     override val headers: ResponseHeaders = object : ResponseHeaders() {
         private val builder = HeadersBuilder()
@@ -39,21 +52,18 @@ class KotlessResponse(call: ApplicationCall) : BaseApplicationResponse(call) {
 
     override suspend fun respondUpgrade(upgrade: OutgoingContent.ProtocolUpgrade) = throw NotImplementedException()
 
-    override suspend fun responseChannel(): ByteWriteChannel {
-        val job = GlobalScope.reader(EmptyCoroutineContext) {
-            channel.copyAndClose(output, Long.MAX_VALUE)
-        }
-        return job.channel
-    }
+    override suspend fun responseChannel() = output
 
     override fun setStatus(statusCode: HttpStatusCode) {
         _status = statusCode
     }
 
     suspend fun toHttp(): HttpResponse {
+        reader.join()
+
         val status = status()?.value ?: 500
         val myHeaders = headers.allValues().entries().map { it.key to it.value.single() }.toMap().let { HashMap(it) }
-        val text = output.toByteArray().toString(Charsets.UTF_8)
+        val text = bytes.toByteArray().toString(Charsets.UTF_8)
         return HttpResponse(status, myHeaders, text, false)
     }
 }
