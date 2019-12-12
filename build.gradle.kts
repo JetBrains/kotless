6c30fc75e0d41a19d20560665971d6d9264ad084
Gregor Billing <suushiemaniac@gmail.com>
2019-12-12 20:01:43 +0100

    Mitigate issues with JNI bindings by updating Kotlin
    See also https://github.com/JetBrains/kotlin/commit/2568804eaa2c8f6b10b735777218c81af62919c1
    
    See also https://github.com/arturbosch/detekt/issues/2118

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ version = "0.1.2"
 plugins {
     id("tanvd.kosogor") version "1.0.7" apply true
     id("io.gitlab.arturbosch.detekt").version("1.0.0-RC14") apply true
-    kotlin("jvm") version "1.3.50" apply false
+    kotlin("jvm") version "1.3.61" apply false
 }
 
 configureIdea {
