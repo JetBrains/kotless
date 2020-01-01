f175ab08b5e999e27a8f1761ea35b2cc46baded6
Vladislav.Tankov <vdtankov@gmail.com>
2020-01-01 22:06:57 +0300

    Update codehaus.plexus dependencies

--- plugins/gradle/build.gradle.kts
+++ plugins/gradle/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 
     implementation("com.github.jengelman.gradle.plugins", "shadow", "5.0.0")
 
-    implementation("org.codehaus.plexus", "plexus-utils", "3.1.1")
-    implementation("org.codehaus.plexus", "plexus-archiver", "4.1.0")
-    implementation("org.codehaus.plexus", "plexus-container-default", "1.0-alpha-30")
+    implementation("org.codehaus.plexus", "plexus-utils", "3.3.0")
+    implementation("org.codehaus.plexus", "plexus-archiver", "4.2.1")
+    implementation("org.codehaus.plexus", "plexus-container-default", "2.1.0")
 }
