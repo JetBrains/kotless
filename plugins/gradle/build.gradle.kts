b69d456ff1c6b0a7c5bc50e56eefa7616608711b
yrusskih <yaroslav.russkih@jetbrains.com>
2021-12-15 17:21:05 +0100

    Update log4j to 2.16.0

--- plugins/gradle/build.gradle.kts
+++ plugins/gradle/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     implementation("com.github.jengelman.gradle.plugins", "shadow", "6.0.0")
     implementation("io.kcdk", "io.kcdk.gradle.plugin", "0.1.1")
 
-    implementation("org.apache.logging.log4j", "log4j-core", "2.15.0")
+    implementation("org.apache.logging.log4j", "log4j-core", "2.16.0")
 
     //local start
     implementation("org.testcontainers", "localstack", "1.16.2")
