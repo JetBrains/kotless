f9dbbddd2e044d6fe2c05f547fc6db3d00b47d10
Maor Hamami <mamaorha@gmail.com>
2023-12-15 16:03:10 +0200

    upgrade dependencies

--- buildSrc/src/main/kotlin/io/kotless/buildsrc/Versions.kt
+++ buildSrc/src/main/kotlin/io/kotless/buildsrc/Versions.kt
@@ -3,21 +3,20 @@ package io.kotless.buildsrc
 object Versions {
     //Note, that it also should be changed in dependencies of buildSrc and in plugins blocks
     //Due to limitations of Gradle DSL
-    const val kotlin = "1.5.31"
-    const val serialization = "1.0.1"
+    const val kotlin = "1.9.21"
+    const val serialization = "1.6.2"
 
-    const val aws = "1.11.788"
-    const val lambdaJavaCore = "1.2.0"
+    const val aws = "1.12.618"
+    const val lambdaJavaCore = "1.2.3"
 
     const val slf4j = "1.7.30"
     const val logback = "1.2.3"
 
-    const val serverlessContainers = "1.5.2"
-
     const val ktor = "1.5.0"
 
-    const val springBoot = "2.4.2"
-    const val spring = "5.3.3"
+    const val serverlessContainers = "1.9.3"
+    const val springBoot = "2.7.18"
+    const val spring = "6.0.11"
 
     const val quartz = "2.3.2"
 }
